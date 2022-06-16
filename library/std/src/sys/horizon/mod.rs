498bb933ea6592478af00082106d6a8bb44b38bf
Nikita Strygin <nikita6@bk.ru>
2022-06-16 20:59:24 +0300

    Make TLS for main thread work (no destructors yet)

--- library/std/src/sys/horizon/mod.rs
+++ library/std/src/sys/horizon/mod.rs
@@ -30,10 +30,10 @@ pub mod pipe;
 pub mod process;
 #[path = "../unsupported/stdio.rs"]
 pub mod stdio;
-#[path = "../unsupported/thread_local_dtor.rs"]
 #[cfg(target_thread_local)]
 pub mod thread_local_dtor;
 
+#[path = "../unsupported/thread_local_key.rs"]
 pub mod thread_local_key;
 #[path = "../unsupported/time.rs"]
 pub mod time;
