f53d03790df0301fa0f6aa41a3ce0fcb9cde5c38
Nikita Strygin <nikita6@bk.ru>
2022-06-16 04:53:53 +0300

    Fuck around with TLS

--- compiler/rustc_target/src/spec/aarch64_nintendo_switch_homebrew.rs
+++ compiler/rustc_target/src/spec/aarch64_nintendo_switch_homebrew.rs
@@ -1,6 +1,6 @@
-use super::{LinkerFlavor, LldFlavor, PanicStrategy, RelroLevel, Target, TargetOptions};
+use super::{LinkerFlavor, LldFlavor, PanicStrategy, RelroLevel, Target, TargetOptions, TlsModel};
 
-const LINKER_SCRIPT: &str = include_str!("./aarch64_nintendo_switch_homebrew_linker_script.ld");
+//const LINKER_SCRIPT: &str = include_str!("./aarch64_nintendo_switch_homebrew_linker_script.ld");
 
 /// A base target for Nintendo Switch devices using a pure LLVM toolchain.
 pub fn target() -> Target {
@@ -12,16 +12,35 @@ pub fn target() -> Target {
         options: TargetOptions {
             linker_flavor: LinkerFlavor::Lld(LldFlavor::Ld),
             linker: Some("rust-lld".into()),
-            link_script: Some(LINKER_SCRIPT.into()),
+            // TODO: enable it back when I figure it out
+            //link_script: Some(LINKER_SCRIPT.into()),
             os: "horizon".into(),
             max_atomic_width: Some(128),
             panic_strategy: PanicStrategy::Abort,
+
+            // we always need PIE (argh, PIE/PIC/whatever stuff is complicated)
             position_independent_executables: true,
+
+            // We don't support dynamic linking for now (it's complicated)
             dynamic_linking: false,
-            main_needs_argc_argv: false,
+            // But we can produce executables
             executables: true,
+
+            // We don't pass argc and argv in horizon-rt
+            main_needs_argc_argv: false,
+
+            // We don't support RELRO
+            // It's something about making .got and .plt read-only I think?
+            // Not sure we care enough, it's just an exploit mitigation
             relro_level: RelroLevel::Off,
+
+            // please emit unwind tables so that I can get the stack traces in debugger
             default_uwtable: true,
+
+            // we do have support for thread-local stuff in horizon-rt & (TODO) on thread creation
+            has_thread_local: true,
+            tls_model: TlsModel::InitialExec,
+
             ..Default::default()
         },
     }
